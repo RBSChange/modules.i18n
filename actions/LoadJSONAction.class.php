25be3a78a709da0957ee739b59e30e9aaf6e8c28
Gaël PORT <gael.port@rbs.fr>
2011-03-01 11:53:28 +0100

    [FIX] Fatal error on folder properties panel.

--- actions/LoadJSONAction.class.php
+++ actions/LoadJSONAction.class.php
@@ -8,7 +8,10 @@ class i18n_LoadJSONAction  extends generic_LoadJSONAction
 	public function _execute($context, $request)
 	{
 		$document = $this->getDocumentInstanceFromRequest($request);
-		$document->setPageindex($request->getParameter('pageindex', 0));
+		if ($document instanceof i18n_persistentdocument_package)
+		{
+			$document->setPageindex($request->getParameter('pageindex', 0));
+		}
 		return parent::_execute($context, $request);
 	}
 }
